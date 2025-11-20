0c15ac97cdde688ddce67e54e2ec4613eb282a5b
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2025-11-20 19:01:56 +0530

    Initial Commit

---
+++ MySchoolTests/MySchoolTests.swift
@@ -0,0 +1,17 @@
+//
+//  MySchoolTests.swift
+//  MySchoolTests
+//
+//  Created by Sajed Shaikh on 20/11/25.
+//
+
+import Testing
+@testable import MySchool
+
+struct MySchoolTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
